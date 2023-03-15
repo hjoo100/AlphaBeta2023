8e0cf634dc0c989abc50f95bfd2fda0899bb343f
Peter <hguanqi7@gmail.com>
2023-03-15 09:56:27 +0800

    Camera Bound

--- Assets/Scripts/scr_camerafollow.cs
+++ Assets/Scripts/scr_camerafollow.cs
@@ -6,6 +6,7 @@ public class scr_camerafollow : MonoBehaviour
 {
     public Transform followTarget;
     public Vector3 camOffset;
+    public Vector2 maxPos, minPos; //for camera bound
     [Range(1, 15)]
     public float smoothVal;
     // Start is called before the first frame update
@@ -26,7 +27,9 @@ public class scr_camerafollow : MonoBehaviour
     void follow()
     {
         Vector3 targetPos = followTarget.position + camOffset;
-        Vector3 smoothedPos = Vector3.Lerp(followTarget.position, targetPos, smoothVal*Time.fixedDeltaTime);
-        transform.position = smoothedPos;
+        targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, minPos.y, maxPos.y);
+        //Vector3 smoothedPos = Vector3.Lerp(followTarget.position, targetPos, smoothVal*Time.fixedDeltaTime);
+        transform.position = targetPos;
     }
 }
