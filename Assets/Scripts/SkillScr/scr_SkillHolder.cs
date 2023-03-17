4802dadfe972105ba2ad19bcc24e84629dd9c512
Peter <hguanqi7@gmail.com>
2023-03-17 21:54:23 +0800

    Skill cooldown indicator

--- Assets/Scripts/SkillScr/scr_SkillHolder.cs
+++ Assets/Scripts/SkillScr/scr_SkillHolder.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scr_SkillHolder : MonoBehaviour
 {
+    public int skillNo = 0;
     public Skill skill;
     float cooldownTime, activeTime;
-
+    [SerializeField]
+    private Image SkillImage;
     enum SkillState
     {
         ready,
@@ -17,9 +20,23 @@ public class scr_SkillHolder : MonoBehaviour
     SkillState state = SkillState.ready;
 
     public KeyCode key;
+
+    private void Start()
+    {
+        if(skillNo == 1)
+        {
+            SkillImage = GameObject.FindGameObjectWithTag("Skill1").GetComponent<Image>();
+        }
+
+        if(skillNo == 2)
+        {
+            SkillImage = GameObject.FindGameObjectWithTag("Skill2").GetComponent<Image>();
+        }
+    }
     // Update is called once per frame
     void Update()
     {
+
         switch(state)
         {
             case SkillState.ready:
@@ -48,6 +65,9 @@ public class scr_SkillHolder : MonoBehaviour
                 if (cooldownTime > 0)
                 {
                     cooldownTime -= Time.deltaTime;
+
+                    float coolDownPercentage = (skill.cooldownTime - cooldownTime) / skill.cooldownTime;
+                    SkillImage.fillAmount = coolDownPercentage;
                 }
                 else
                 {
