e974396c6428d61d11c478a5e99e4da732bd7130
Peter <hguanqi7@gmail.com>
2023-05-06 14:39:15 +1200

    Javelin barrage skill and tool tip updates.

---
+++ Assets/Scripts/Enemy/Scr_JavelinBarrageObj.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_JavelinBarrageObj : MonoBehaviour
+{
+    public float arrowAngleOffset = 15f;
+    public GameObject bullet;
+
+    AudioSource audioSrc;
+    public AudioClip firesound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        FireJavelin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireJavelinByTimes(int time)
+    {
+        for(int i = 0; i<time; i++)
+        {
+            FireJavelin();
+            
+        }
+    }
+    public void FireJavelin()
+    {
+        float[] angles = { -2 * arrowAngleOffset, -arrowAngleOffset, 0f, arrowAngleOffset, 2 * arrowAngleOffset };
+        float initialForce = 5f;
+
+        foreach (float angle in angles)
+        {
+            GameObject arrowInstance = Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, 0));
+            Quaternion arrowRotation = transform.rotation * Quaternion.Euler(0, 0, angle + 135f);
+            arrowInstance.transform.rotation = arrowRotation * Quaternion.Euler(0, 0, 22.5f);
+
+            Vector3 adjustedRight = Quaternion.Euler(0, 180, 0) * arrowInstance.transform.right;
+            arrowInstance.transform.right = adjustedRight;
+
+            Rigidbody2D arrowRb = arrowInstance.GetComponent<Rigidbody2D>();
+            arrowRb.AddForce(arrowInstance.transform.up * initialForce, ForceMode2D.Impulse);
+        }
+
+        audioSrc.clip = firesound;
+        audioSrc.Play();
+    }
+}
---
+++ Assets/Scripts/SkillScr/EnemySkill/EnemyJavelinBarrageSkill.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJavelinBarrageSkill : Skill
+{
+
+    private GameObject JavelinLauncher1,JavelinLauncher2;
+
+
+
+    protected EnemyJavelinBarrageSkill(string name, SkillEnum.SkillType skillType, int level) : base(name, skillType, level)
+    {
+        base.name = name;
+        base.skillType = skillType;
+        base.Level = level;
+    }
+
+    public bool IsSkillActive { get; private set; }
+
+    public override void Initialize(string name, SkillEnum.SkillType skillType, int level)
+    {
+        base.Initialize(name, skillType, level);
+
+        JavelinLauncher1 = GameObject.FindWithTag("JavelinLauncher1");
+        JavelinLauncher2 = GameObject.FindWithTag("JavelinLauncher2");
+
+    }
+
+    public override void ActivateSkill(GameObject obj)
+    {
+        base.ActivateSkill(obj);
+        JavelinLauncher1.GetComponent<Scr_JavelinBarrageObj>().FireJavelin();
+
+        JavelinLauncher2.GetComponent<Scr_JavelinBarrageObj>().FireJavelin();
+    }
+
+    public override void StartSkillCD(GameObject obj)
+    {
+        base.StartSkillCD(obj);
+        
+    }
+
+
+    
+
+    
+
+
+}
